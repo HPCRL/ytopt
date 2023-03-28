39bcba1b2895704768eff2e052918b8c9e4bf6f7
Sameeranjoshi <joshisameeran17@gmail.com>
2023-03-28 00:40:29 -0600

    Tried various approaches to call a shell command from python script, the final and best and simple but dirty approach is to use os.system this works at least to some extent and is more readable compared to other approaches whereas, iree-run command fails as some issue with CUDA drivers

--- ytopt/benchmark/mmm-block/plopper/plopper_amir_transformdialect_test.py
+++ ytopt/benchmark/mmm-block/plopper/plopper_amir_transformdialect_test.py
@@ -18,12 +18,12 @@ class Plopper:
         for p, v in zip(params, x):
             dictVal[p] = v
         return(dictVal)
-    
+
     # Function to find the execution time of the interim file, and return the execution time as cost to the search module
     def findRuntime(self, x, params):
         interimfile = ""
         exetime = 1
-        
+
         # Generate intermediate file
         dictVal = self.createDict(x, params)
         sourcefile_dir = output_dir_path = os.path.dirname(os.path.realpath(self.sourcefile))
@@ -48,51 +48,79 @@ class Plopper:
         os.chdir(sourcefile_dir + "/build")
 
 
-#        
+#
 #        compile_cmd = "cd /uufs/chpc.utah.edu/common/home/u1418973/other/llvm_stuff/IREE/htd-codegen/build &&"
 #        compile_cmd = "cmake -DCMAKE_CXX_FLAGS=\"-DAUTO_TUNER -DBX={0} -DBY={1} -DTY={2} -DTX={3}\" ..".format(dictVal['BX'], dictVal['BY'], dictVal['TX'], dictVal['TY'])
 #        compile_cmd += " && make && ./codegen "
-#        compile_cmd +=" && iree-compile " + payload_ir_file + " --iree-hal-target-backends=cuda --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --iree-codegen-llvmgpu-enable-transform-dialect-jit=false --iree-codegen-llvmgpu-use-transform-dialect=" + lowlevel_transform_ir_file + " &> " + tmpvmfb      
+#        compile_cmd +=" && iree-compile " + payload_ir_file + " --iree-hal-target-backends=cuda --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --iree-codegen-llvmgpu-enable-transform-dialect-jit=false --iree-codegen-llvmgpu-use-transform-dialect=" + lowlevel_transform_ir_file + " &> " + tmpvmfb
 #        run_cmd = 'iree-run-module --function=linalg_matmul --device=cuda --input=\"1024x128xf32=1\" --input=\"128x2048xf32=1\" --input=\"1024x2048xf32=0\" --module=' + tmpvmfb        #+ tmpoutput
 
-        compile_cmd = 'cmake -DCMAKE_CXX_FLAGS=-DAUTO_TUNER -DBX={0} -DBY={1} -DTY={2} -DTX={3} ..'
-        build_cmd = 'make'
-        run = './codegen'
-        #compile_cmd += 'make && ./codegen'
-        
-        #&& make && ./codegen && iree-compile {4} --iree-hal-target-backends=cuda --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --iree-codegen-llvmgpu-enable-transform-dialect-jit=false'.format(dictVal['BX'], dictVal['BY'], dictVal['TX'], dictVal['TY'], payload_ir_file) #, lowlevel_transform_ir_file, tmpvmfb)
+        build_cmd = "cmake -DCMAKE_CXX_FLAGS=\"-DAUTO_TUNER -DBX={0} -DBY={1} -DTY={2} -DTX={3}\" .. && make && ./codegen".format(dictVal['BX'], dictVal['BY'], dictVal['TX'], dictVal['TY'])
+        compile_cmd = 'iree-compile {0} --iree-hal-target-backends=cuda --iree-opt-const-expr-hoisting=false --iree-opt-const-eval=false --iree-codegen-llvmgpu-enable-transform-dialect-jit=false --iree-codegen-llvmgpu-use-transform-dialect={1} &> {2}'.format(payload_ir_file, lowlevel_transform_ir_file, tmpvmfb)
+        run_cmd = 'iree-run-module --function=linalg_matmul --device=cuda --input=\"1024x128xf32=1\" --input=\"128x2048xf32=1\" --input=\"1024x2048xf32=0\" --module=\"{0}\" '.format(tmpvmfb)
 
-        #--iree-codegen-llvmgpu-use-transform-dialect={5} &> {6}'.format(dictVal['BX'], dictVal['BY'], dictVal['TX'], dictVal['TY'], payload_ir_file, lowlevel_transform_ir_file, tmpvmfb)
-        run_cmd = "iree-run-module --function=linalg_matmul --device=cuda --input=\"1024x128xf32=1\" --input=\"128x2048xf32=1\" --input=\"1024x2048xf32=0\" --module=\"{0}\"".format(tmpvmfb)
-
-        
-        print (compile_cmd.split())
+        with open("filename.sh", "w") as file: # xyz.txt is filename, w means write format
+          file.write("#!/bin/bash")
+          file.write('\n')
+          file.write(run_cmd) # write text xyz in the file
+    
 
 #######################################################################
 
-# Execute the compile command
-        compile_process = subprocess.Popen(compile_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        compile_process = subprocess.Popen(build_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        compile_process = subprocess.Popen(run, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        compile_output, compile_error = compile_process.communicate()
-        if compile_process.returncode != 0:
-            # Compilation failed
-            print("Compilation failed with error:")
-            print(compile_error.decode())
-        else:
-            # Compilation succeeded, execute the run command
-            run_process = subprocess.Popen(run_cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            run_output, run_error = run_process.communicate()
-            if run_process.returncode != 0:
-                # Run failed
-                print("Run failed with error:")
-                print(run_error.decode())
-            else:
-                # Run succeeded, print the output
-                print(run_output.decode())
-        return exetime
+## Execute the compile command
+#        build_process = subprocess.run(build_cmd, shell=True)
+#        build_output, build_error = build_process.communicate()
+#        if build_process.returncode != 0:
+#            # build failed
+#            print("Build failed with error:")
+#            print(build_error.decode())
+#        else:
+#            # build succeeded, execute the compile command
+#            print ("BUILD_SUCCESS")
+#            compile_process = subprocess.run(compile_cmd, shell=True)
+#            compile_output, compile_error = compile_process.communicate()
+#            if compile_process.returncode != 0:
+#                # Compilation failed
+#                print("Compile command failed with error:")
+#                print(compile_error.decode())
+#            else:
+#                print ("COMPILE_SUCCESS")
+#                # compile success now run.
+#                run_process = subprocess.Popen(run_cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#                run_output, run_error = run_process.communicate()
+#                if run_process.returncode != 0:
+#                    # Run failed
+#                    print("Run failed with error:")
+#                    print(run_error.decode())
+#                else:
+#                    # Run succeeded, print the output
+#                    print(run_output.decode())
+#        return exetime
 ######################################################################33
+# os.system
+
+# Execute the command in a subshell using os.system
+        print (build_cmd)
+        print (compile_cmd)
+        print (run_cmd)
+        return_value = os.system(build_cmd)
+        if return_value == 0:
+            print("Build Command executed successfully")
+            compile_return_value = os.system(compile_cmd)
+            if compile_return_value == 0:
+                print("Compile Command executed successfully")
+                run_return_value = os.system(run_cmd)
+                os.system("bash filename.sh")
+                if run_return_value == 0:
+                    print("Run Command executed successfully")
+                else:
+                    print("Run Command failed with exit status:", os.WEXITSTATUS(run_return_value))
+            else:
+                print("Compile Command failed with exit status:", os.WEXITSTATUS(compile_return_value))
+        else:
+            print("Build Command failed with exit status:", os.WEXITSTATUS(return_value))
+######################################################################33
+
 #        #Find the compilation status using subprocess
 #        compilation_status = subprocess.run(compile_cmd, shell=True, stderr=subprocess.PIPE)
 #
@@ -100,7 +128,7 @@ class Plopper:
 #        if compilation_status.returncode == 0 :
 #            print ("COMPILED SUCCESS")
 #            print (run_cmd)
-#            
+#
 ##            execution_status = subprocess.run(run_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #            execution_status = subprocess.run(run_cmd, shell=True, stdout=subprocess.PIPE)
 #            exetime = float(execution_status.stdout.decode('utf-8'))
@@ -116,3 +144,4 @@ class Plopper:
 #            print("compile failed")
 #        return exetime #return execution time as cost
 #
+                                                                                                                                 
