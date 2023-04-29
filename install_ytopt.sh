5d103d316ecfef875bc16ab66c9c32dc6f7de50c
Sameeranjoshi <joshisameeran17@gmail.com>
2023-04-29 11:35:52 -0600

    [script] bash scripts to install ytopt.
    you really don't need to clone ytopt as it would be cloned through script, just get this
    script in a clean folder and run, it builds and installs everything for you.

---
+++ install_ytopt.sh
@@ -0,0 +1,19 @@
+mkdir ytopt
+cd ytopt
+git clone https://github.com/ytopt-team/ConfigSpace.git
+cd ConfigSpace
+module load python/3.10
+pip3.10 install -e .
+cd ..
+git clone https://github.com/ytopt-team/scikit-optimize.git
+cd scikit-optimize
+pip3.10 install -e .
+cd ..
+git clone -b version1 https://github.com/ytopt-team/autotune.git
+cd autotune
+pip3.10 install -e .
+cd ..
+module load gcc openmpi
+git clone -b main https://github.com/ytopt-team/ytopt.git
+cd ytopt
+pip3.10 install -e .
