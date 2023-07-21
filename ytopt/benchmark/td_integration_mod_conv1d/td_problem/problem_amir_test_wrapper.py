c2dda148b0773ce37721f0b6f3cd7edd335ab1db
Sameeranjoshi <joshisameeran17@gmail.com>
2023-07-21 13:28:40 -0600

    conv1d changes added

---
+++ ytopt/benchmark/td_integration_mod_conv1d/td_problem/problem_amir_test_wrapper.py
@@ -0,0 +1,115 @@
+import json
+import numpy as np
+from autotune import TuningProblem
+from autotune.space import *
+import os, sys, time, json, math
+import ConfigSpace as CS
+import ConfigSpace.hyperparameters as CSH
+from skopt.space import Real, Integer, Categorical
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(1, os.path.dirname(HERE) + "/plopper")
+from plopper_amir_transformdialect_test import Plopper
+
+##############################Automate the inputs from json################################################
+input_json_file_name = "input_config.json"
+# Read the JSON input file
+with open(input_json_file_name, "r") as f:
+    input_data = json.load(f)
+##############################Bookkeeping part#################################
+bookkeeping = input_data["bookkeeping"]
+autotuner_target_dir = bookkeeping["autotuner_target_dir"]
+autotuner_highlevel_td_spec_file = bookkeeping["autotuner_highlevel_td_spec_file"]
+
+output_dir_path = os.path.dirname(os.path.realpath(__file__))
+obj = Plopper(
+    autotuner_target_dir + "/" + autotuner_highlevel_td_spec_file,
+    output_dir_path,
+    bookkeeping,
+)
+##############################Configuration#####################################
+
+# Create a ConfigurationSpace object
+cs = CS.ConfigurationSpace()
+
+# Add hyperparameters to the ConfigurationSpace object
+for param in input_data["configuration"]:
+    name_t = param["name"]
+    type_t = param["type"]
+
+    if type_t == "integer":
+        lower_t = int(param["lower"])
+        upper_t = int(param["upper"])
+        default_value_t = int(param["default_value"])
+        cs.add_hyperparameter(
+            CSH.UniformIntegerHyperparameter(name_t, lower_t, upper_t, default_value_t)
+        )
+    elif type_t == "float":
+        lower_t = float(param["lower"])
+        upper_t = float(param["upper"])
+
+        default_value_t = float(param["default_value"])
+        cs.add_hyperparameter(
+            CSH.UniformFloatHyperparameter(name_t, lower_t, upper_t, default_value_t)
+        )
+    elif type_t == "categorical":
+        if "choices" not in param:
+            raise ValueError(
+                "Input error: 'choices' key is missing for 'categorical' type"
+            )
+        listp = param["choices"]
+        # default_value_t = param["default_value"]
+        cs.add_hyperparameter(
+            CSH.CategoricalHyperparameter(
+                name=name_t, choices=listp
+            )
+        )
+    else:
+        print("Unknown 'type' value from" + input_json_file_name + " file")
+        sys.exit(1)
+##############################################################################
+
+# problem space
+task_space = None
+input_space = cs
+output_space = Space([Real(0.0, inf, name="time")])
+i = 0
+
+############################################################################
+# The input parameters setting is done by now, let's into the main code now.
+############################################################################
+def myobj(point: dict):
+    global i
+    def plopper_func(x):
+        x = np.asarray_chkfinite(x)  # ValueError if any NaN or Inf
+        value = list(point.values())
+        print("CONFIG:", point)
+        txvalue = point["TOH"] * point["TOW"]
+        tyvalue = point["TN"] * point["TF"]
+        bxvalue = point["BOH"] * point["BOW"]
+        byvalue = point["BN"] * point["BF"]
+
+        totalthreads = txvalue * tyvalue
+        params = {k.upper(): v for k, v in point.items()}
+        if bxvalue * byvalue >= 4 and txvalue * tyvalue >=16 and totalthreads <= 1024 and totalthreads > 0:
+            result = obj.findRuntime(value, params)
+        else:
+            result = float('+inf')
+        return result
+
+    x = np.array(list(point.values()))
+    results = plopper_func(x)
+    print(f"OUTPUT_{i}: ", results)
+    i = i + 1
+    return results
+
+
+##############################Pass all inputs to tuning####################
+Problem = TuningProblem(
+    task_space=None,
+    input_space=input_space,
+    output_space=output_space,
+    objective=myobj,
+    constraints=None,
+    model=None,
+)
---
+++ ytopt/benchmark/td_integration_mod_conv1d/td_problem/run.sh
@@ -0,0 +1,15 @@
+#!/bin/csh
+#SBATCH --time=12:00:00 # walltime, abbreviated by -t
+#SBATCH -o matmul_timing_%j.sout # name of the stdout, using the job number (%j) and the first node (%N)
+#SBATCH -e matmul_timing_%j.serr # name of the stderr, using job and first node values
+#SBATCH --ntasks=16    # number of MPI tasks, abbreviated by -n
+#SBATCH --account=soc-gpu-np     # account - abbreviated by -A
+#SBATCH --partition=soc-gpu-np  # partition, abbreviated by -p
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem=32000
+#SBATCH --exclude=notch369,notch370   # excluding nodes notch369 and notch370
+
+module load cuda python/3.10
+
+python3 -W ignore -m ytopt.search.ambs --evaluator ray --problem problem_amir_test_wrapper.py --max-evals=1000 --learner GBRT
+python3 findMin.py
